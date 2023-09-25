fe9e5fc46fcf2e81dcf946c61519ef1fcefdbdfb
Jason Wall <javajo@gmail.com>
2023-09-24 21:36:46 -0600

    I can't believe that worked

--- tests/dsl.test.ts
+++ tests/dsl.test.ts
@@ -1,7 +1,7 @@
 import { describe } from "@jest/globals";
 import { PromiseMock } from "@self/PromiseMock";
 import dsl from "@self/dsl";
-import { mockFn } from "./utils/assorted";
+import { expectAll, mockFn } from "./utils/assorted";
 
 function assignCallbacks<T>(p: PromiseMock<T>) {
   const handlers = mockFn("then1", "catch1", "finally1");
@@ -11,13 +11,6 @@ function assignCallbacks<T>(p: PromiseMock<T>) {
   return handlers;
 }
 
-function expectAll<T>(
-  definer: (matcher: jest.JestMatchers<T>) => void,
-  ...values: T[]
-) {
-  for (const val of values) definer(expect(val));
-}
-
 describe("promitto", () => {
   describe("#pending", () => {
     it("should return a pending promise that represents the provided value", async () => {
@@ -25,13 +18,13 @@ describe("promitto", () => {
       const handlers = assignCallbacks(p);
       const [then1, catch1, finally1] = handlers;
 
-      expectAll((h) => h.not.toHaveBeenCalled(), ...handlers);
+      expectAll(...handlers).not.toHaveBeenCalled();
 
       p.resolve();
       const result = await p.settled();
 
       expect(result).toEqual("a nice value");
-      expectAll((h) => h.toHaveBeenCalledTimes(1), then1, finally1);
+      expectAll(then1, finally1).toHaveBeenCalledTimes(1);
       expect(catch1).not.toHaveBeenCalled();
     });
   });
@@ -42,12 +35,12 @@ describe("promitto", () => {
       const handlers = assignCallbacks(p);
       const [then1, catch1, finally1] = handlers;
 
-      expectAll((h) => h.not.toHaveBeenCalled(), ... handlers);
+      expectAll(... handlers).not.toHaveBeenCalled();
 
       p.resolve(11);
       const result = await p.settled();
 
-      expectAll((h) => h.toHaveBeenCalledTimes(1), then1, finally1);
+      expectAll(then1, finally1).toHaveBeenCalledTimes(1);
       expect(then1).toHaveBeenCalledWith(11);
       expect(catch1).not.toHaveBeenCalled();
     });
@@ -57,12 +50,12 @@ describe("promitto", () => {
       const handlers = assignCallbacks(p);
       const [then1, catch1, finally1] = handlers;
 
-      expectAll((h) => h.not.toHaveBeenCalled(), ...handlers);
+      expectAll(...handlers).not.toHaveBeenCalled();
 
       p.reject(new Error('rejected'));
       await expect(p.settled()).rejects.toThrowError('rejected');
 
-      expectAll((h) => h.toHaveBeenCalledTimes(1), catch1, finally1);
+      expectAll(catch1, finally1).toHaveBeenCalledTimes(1);
       expect(catch1).toHaveBeenCalledWith(new Error('rejected'));
       expect(then1).not.toHaveBeenCalled();
     });
--- tests/utils/assorted.ts
+++ tests/utils/assorted.ts
@@ -4,4 +4,22 @@ function mockFn(...fns: string[]): jest.Mock[] {
   return results;
 }
 
-export { mockFn };
+function expectAll<T>(
+  ...values: T[]
+): jest.JestMatchers<T> {
+  const results: any[] = values.map((x) => expect(x));
+  const handler: ProxyHandler<jest.JestMatchers<T>> = {
+    get: (target, propName) => {
+      for (let i = 0; i < results.length; i++) {
+        results[i] = results[i][propName];
+      }
+      return new Proxy((target as any)[propName], handler);
+    },
+    apply: (_, __, argArray) => {
+      for (const result of results) result(...argArray);
+    }
+  }
+  return new Proxy<jest.JestMatchers<T>>(expect<T>(values[0]), handler);
+}
+
+export { mockFn, expectAll };
